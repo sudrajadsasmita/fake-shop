d72ad68d66833368f657ed5d2d389607f490ccb4
samsulbahari <samsul.bahari23@gmail.com>
2024-08-20 08:52:40 +0700

    fix clean architecture

--- lib/features/product/domain/repositories/product_repository.dart
+++ lib/features/product/domain/repositories/product_repository.dart
@@ -5,4 +5,5 @@ import 'package:fake_shop/features/product/domain/entities/product.dart';
 abstract class ProductRepository {
   Future<Either<Failure, List<Product>>> getProducts();
   Future<Either<Failure, Product>> getProductDetail(String id);
+  Future<Either<Failure, Product>> getProductDetail(String id);
 }
